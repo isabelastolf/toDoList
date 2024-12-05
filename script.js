37915bd41f061a942e3b4a4114079bdda42d6344
Isabela <isabelaguzzardi@gmail.com>
2024-12-05 11:17:19 -0300

    To Do List written in HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,32 @@
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("O campo não pode estar vazio");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+        <div class="task">
+            <span id="taskname">
+                ${document.querySelector("#newtask input").value}
+            </span>
+            <button class="delete">
+                <ion-icon name="checkmark-sharp"></ion-icon>
+            </button>
+        </div>
+    `;
+
+    let current_tasks = document.querySelectorAll(".delete");
+    for (let i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+
+    let tasks = document.querySelectorAll(".task");
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+
+    document.querySelector("#newtask input").value = "";
+  }
+};
